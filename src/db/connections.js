fb73b332532c42996710392d8bba8f10334aab27
Arun <arun.n61@gmail.com>
2022-12-22 20:19:02 +0530

    update

--- src/db/connections.js
+++ src/db/connections.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const connectionObj = mongoose.connect("mongodb+srv://reporting:reporting@cluster0.u6nrmx5.mongodb.net/test", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 module.exports = { connectionObj };
 
