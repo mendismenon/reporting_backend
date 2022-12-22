279d411ce7016c66813af43c1e631e6a7bf33278
Arun <arun.n61@gmail.com>
2022-12-22 20:48:51 +0530

    update

--- src/routers/formData.js
+++ src/routers/formData.js
@@ -2,16 +2,15 @@ const express = require("express");
 const { uploadFormConnection } = require("../db/modals/formdata");
 const formDataRouter = new express.Router();
 
+formDataRouter.post("/tasks", async (req, res) => {
+  const newTask = new uploadFormConnection(req.body);
+  try {
+    const resObj = await newTask.save();
+    res.status(200).send(resObj);
+  } catch (e) {
+    console.log(e);
+    res.status(400).send({ data: e, object: resObj, newTask:newTask });
+  }
+});
 
-formDataRouter.post("/tasks", async (req,res)=>{
-    const newTask = new uploadFormConnection(req.body);
-    try{
-      const resObj = await newTask.save();
-      res.status(200).send(resObj);
-    }catch(e){
-        console.log(e);
-      res.status(400).send({data:e});
-    }
-  })
-
-  module.exports = {formDataRouter};
+module.exports = { formDataRouter };
